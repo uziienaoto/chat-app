40a2d0c5aa07008836177911ebf859f6e558eab1
uziienaoto <otoaneiizu@gmail.com>
2020-07-24 17:19:33 +0900

    messagesテーブルの作成

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,7 @@ Rails.application.routes.draw do
   get 'messages/index'
   root "messages#index"
   resources :users, only: [:edit, :update]
+  resources :rooms, only: [:new, :create] do
+    resources :messages, only: [:index, :create]
+
 end
